1da23faea9128ef7d6ce08f3a7aaf8e251ef3abb
GeneralGak <martino96@live.dk>
2026-04-10 11:40:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,9 +43,9 @@ function InitViewport()
 
     // Initialize pixel buffer properties
     graphicsLibrary.clearColor(0.0, 0.4, 0.6, 1.0);
-    graphicsLibrary.enable(gl.DEPTH_TEST);
-    graphicsLibrary.enable(gl.CULL_FACE);
-    graphicsLibrary.cullFace(gl.BACK);
+    graphicsLibrary.enable(graphicsLibrary.DEPTH_TEST);
+    graphicsLibrary.enable(graphicsLibrary.CULL_FACE);
+    graphicsLibrary.cullFace(graphicsLibrary.BACK);
     //InitShaders();
 }
 
